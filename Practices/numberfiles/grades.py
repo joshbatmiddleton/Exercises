b4cf1d7451ddbf56075af9000a5f25a801b00d87
joshbatmiddleton <98915670+joshbatmiddleton@users.noreply.github.com>
2022-02-14 12:41:10 +1300

    Update v1.1

--- Practices/numberfiles/grades.py
+++ Practices/numberfiles/grades.py
@@ -1,1 +1,9 @@
-Mark =
+mark = int(input("Please enter mark: "))
+if mark >= 90:
+    print(f"{mark} is grade A! Well done!")
+elif mark >= 70:
+    print(f"{mark} is a B! Good on you!")
+elif mark >= 50:
+    print(f"{mark} is a C! Keep trying!")
+else:
+    print(f"{mark} is a fail. See me after school")
