3c48fcec5981fb3bb20104af55f1e06d0cd0cfcd
joshbatmiddleton <98915670+joshbatmiddleton@users.noreply.github.com>
2022-02-15 15:14:37 +1300

    Update v1.2

--- Practices/practice3.py
+++ Practices/practice3.py
@@ -1,7 +1,7 @@
 number = int(input("Enter number: "))
 
-if number % 2 == 0
-    print(f"{number} is even)
+if number % 2 == 0:
+    print(f"{number} is even")
 else:
     print(f"{number} is odd")
 
