270b6a9ee0ba37e88550b887587a497e948cc0ff
joshbatmiddleton <98915670+joshbatmiddleton@users.noreply.github.com>
2022-02-21 12:21:00 +1300

    Update v1.4.1

--- Practices/numberfiles/guess_number.py
+++ Practices/numberfiles/guess_number.py
@@ -1,7 +1,15 @@
 import random
-number = random.randint(0, 10)
+number = random.randint(0, 100)
+count = 0
 
 guess = int(input("Guess the number: "))
-while guess != number:
-    guess = int(input("Wrong. Try again: "))
-print("You got it!")
+while guess != number and count < 10:
+    count += 1
+    if guess < number:
+        guess = int(input("That's too low, try again: "))
+    else:
+        guess = int(input("That's too high, try again: "))
+if guess == number:
+    print(f"You got it in {count} guesses!")
+else:
+    print("You have used up all of your guesses")
