1df3e4068fc8ad123122d9ae62215ec95da929e8
joshbatmiddleton <98915670+joshbatmiddleton@users.noreply.github.com>
2022-02-21 12:35:24 +1300

    Update v1.4.2

--- Practices/numberfiles/guess_number.py
+++ Practices/numberfiles/guess_number.py
@@ -1,9 +1,21 @@
 import random
 number = random.randint(0, 100)
-count = 0
+count = 1
+mode = ""
+while mode != "H" and mode != "M" and mode != "E":
+    mode = input("Enter 'E' for Easy mode, 'M' for Medium mode or 'H' for Hard mode: ")
+    if mode == "H":
+        num_guesses = 3
+    elif mode == "M":
+        num_guesses = 5
+    elif mode == "E":
+        num_guesses = 10
+    else:
+        print("That was not an option. Try again: ")
 
 guess = int(input("Guess the number: "))
-while guess != number and count < 10:
+while guess != number and count < num_guesses:
+    
     count += 1
     if guess < number:
         guess = int(input("That's too low, try again: "))
@@ -12,4 +24,4 @@ while guess != number and count < 10:
 if guess == number:
     print(f"You got it in {count} guesses!")
 else:
-    print("You have used up all of your guesses")
+    print(f"You have used your guesses")
