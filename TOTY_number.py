2ac119166d0b4ade321d34617dab181c7163a578
joshbatmiddleton <98915670+joshbatmiddleton@users.noreply.github.com>
2022-03-23 14:07:30 +1300

    Update v1.5.13
    Last Update of v1.5. Will also be the last update for a while

--- TOTY_number.py
+++
@@ -1,8 +0,0 @@
-count = 0
-while count < 10:
-    team_of_the_year = input("Enter the item you got: ")
-    if team_of_the_year != "":
-        count += 1
-        print(count)
-    else:
-        print("You are guaranteed to get a Team Of The Year in your next pack!")
